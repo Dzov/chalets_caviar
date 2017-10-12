136b218f97c1131c41c6ae7202834b89b2cf71ca
Dzov <amelie.haladjian@gmail.com>
2017-10-12 21:50:28 +0200

    refactor(*): remove unused pluggins

--- wp-content/plugins/real-estate-listing-realtyna-wpl/assets/styles/divi/main.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by obolo on 29.04.2017.
- */
--- wp-content/plugins/wp-immo/languages/infos.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Just a few strings for translate plugin informations
- */
-$plugin_infos = array(
-    __( 'Manage your real estate in WordPress.', WPIMMO_PLUGIN_NAME ),
-);
