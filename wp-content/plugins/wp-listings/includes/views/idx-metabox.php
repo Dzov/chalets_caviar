bbd6a35dab837a249d041abe28693f96bf6c75d9
Dzov <amelie.haladjian@gmail.com>
2017-10-13 13:02:07 +0200

    feat(plugins): add secure and backup plugins

--- wp-content/plugins/wp-listings/includes/views/idx-metabox.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo '<a href="http://www.idxbroker.com/features/idx-wordpress-plugin" target="_blank"><img style="max-width: 100%;" src="' . WP_LISTINGS_URL . 'images/idx-ad.png" alt="Hook up your MLS with IDX Broker"></a>';
